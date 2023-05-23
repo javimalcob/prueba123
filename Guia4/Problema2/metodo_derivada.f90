8c9e0e3655cfa5a121ab18b9d7785b141414bc57
Javier <javier.mercado.alcoba@mi.unc.edu.ar>
2023-05-23 16:45:14 -0300

    lord valdomero

--- Guia4/Problema2/metodo_derivada.f90
+++ Guia4/Problema2/metodo_derivada.f90
@@ -4,13 +4,45 @@ use funciones
 
 implicit none 
 contains
+
+!-------------------------------------------------------------------------------------
+	subroutine derivada3extremo(x, h, f, dfc)
+	    !Declaracion de dummy variables
+	    real(wp), intent(in)                :: c 
+	    real(wp), intent(in)                :: h 
+	    real(wp), intent(in)                :: f
+	    real(wp), intent(out)               :: dfc
+	    !Declaracion de variables auxiliares
+	    
+	    dfc = 1._wp/2._wp*h * (-3._wp*f(x) + 4._wp*f(c + h) -2._wp*f(c + 2._wp*h))
+	
+	end subroutine derivada3extremo
+	
+!-------------------------------------------------------------------------------------	
+	
 	subroutine derivada3centrada(c, h, f, dfc)
-	
-	
-	
+	    !Declaracion de Dummy varaibles
+	    real(wp), intent(in)            :: c
+        real(wp), intent(in)            :: h
+        real(wp), intent(in)            :: f
+        real(wp), intent(out)           :: dfc 	
+	    
+	    dfc = 1._wp/2_wp*h * (f(c - h) - f(c + h))
 	
 	end subroutine derivada3centrada
+!-------------------------------------------------------------------------------------
 	
+	subroutine derivada5extremo(c, h, f, dfc)
+	    !Declaracion de dummy variables
+	    real(wp), intent(in)            :: c
+        real(wp), intent(in)            :: h
+        real(wp), intent(in)            :: f
+        real(wp), intent(out)           :: dfc
+        
+        dfc = 1._wp/12._wp*h*(-25._wp*f(c) + 48._wp * f(c + h)- 36._wp * f(c + 2._wp*h) + 16._wp * f(c + 3._wp*h) - 3._wp*f(c + 4._wp*h))
+	end subroutine derivada5extremo
+	
+!-------------------------------------------------------------------------------------	
 	
 	subroutine derivada5centrada(c, h ,f, dfc)
         !Declaracion de Dummy variables
@@ -21,10 +53,10 @@ contains
 	    
 	    !Declaracion de variables auxiliares
 	    
-	    dfc = 1/(12*h)*(f(c - 2*h) - 8*(c-h) + 8*(c + h) - f(c + 2*h))	
+	    dfc = 1._wp/(12._wp*h)*(f(c - 2._wp*h) - 8._wp*(c-h) + 8._wp*(c + h) - f(c + 2._wp*h))	
 	    
 	end subroutine derivada5centrada
 
-
+!-------------------------------------------------------------------------------------
 
 end module derivadas
